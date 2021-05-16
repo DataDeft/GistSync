75a2118b9464a4053ffbbc0e2c30dbcbdc26ba2a
Istvan <istvan@lambdainsight.com>
2021-05-16 11:34:41 +0200

    Working auth with token

--- Main.fs
+++ Main.fs
@@ -7,10 +7,13 @@ open System
 
 // External
 
+open FSharp.Data
+open FSharp.Data.HttpRequestHeaders
 open Newtonsoft.Json
 open Newtonsoft.Json.Serialization
 open System.IO
 open System.Collections.Generic
+open System
 
 
 module Main =
@@ -20,14 +23,53 @@ module Main =
     System.Console.WriteLine s
 
 
+  let readPersonalToken () =
+    try
+      let home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+      let token = (File.ReadAllText (Path.Combine (List.toArray [home; ".git"; "githubToken"])))
+      let trimmedToken = token.TrimEnd('\r', '\n')
+      Some trimmedToken
+    with ex ->
+      logger <| sprintf "Could not read token file: %A" ex.Message
+      logger <| sprintf "Create a file in ~/.git/ with the name githubToken"
+      None
+
+
+  let httpValami userName token =
+
+    let authString =
+      "token " + token
+
+    let httpString =
+      Http.AsyncRequestString
+        ("https://api.github.com/user/repos", httpMethod = "GET",
+        query   = [  ],
+        headers = [
+          Authorization authString
+          Accept HttpContentTypes.Json
+          UserAgent "GistSync"
+        ])
+
+    async {
+      try
+        let! resp = httpString
+        logger <| sprintf "%A" resp
+      with ex ->
+        logger <| sprintf "Error happened while talking to Github API: %s" ex.Message
+    }
+    |> Async.RunSynchronously
+
+
   let syncGists localPath =
+    readPersonalToken()
+    |> Option.map (httpValami "l1x")
+    |> Option.defaultWith (fun x -> logger <| sprintf "%A" x)
 
-    
-
-    ()
 
   let listGists localPath =
+
     ()
+
 
   [<EntryPoint>]
   let main args =
