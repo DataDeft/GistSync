d80606c04136593a4e7e4975c893584610845bbb
Istvan <istvan@lambdainsight.com>
2021-05-17 10:24:22 +0200

    Updating readme

--- Main.fs
+++ Main.fs
@@ -14,6 +14,34 @@ open Newtonsoft.Json.Serialization
 open System.IO
 open System.Collections.Generic
 open System
+open System.Text.RegularExpressions
+open System.Text
+
+
+type FileEntry = {
+  //Filename: string
+  RawUrl: string
+}
+
+type Gist = {
+  Url: string
+  Id: string
+  Files: Dictionary<string, FileEntry>
+  CreatedAt: string
+  UpdatedAt: string
+}
+
+[<StructuredFormatDisplay("Limit: {Limit} :: Used: {Used} Remaining: {Remaining} :: Reset: {Reset}" )>]
+type RateLimitDetails = {
+  Limit: int
+  Used: int
+  Remaining: int
+  Reset: int
+}
+
+type RateLimit = {
+  Rate: RateLimitDetails
+}
 
 
 module Main =
@@ -21,6 +49,10 @@ module Main =
 
   let logger (s:string) =
     System.Console.WriteLine s
+
+
+  let getWaitTimeExp (retryCount: int) =
+    int (Math.Pow(2.0, (float retryCount)) * 100.0)
 
 
   let readPersonalToken () =
@@ -35,35 +67,232 @@ module Main =
       None
 
 
-  let httpValami userName token =
-
-    let authString =
-      "token " + token
-
-    let httpString =
-      Http.AsyncRequestString
-        ("https://api.github.com/user/repos", httpMethod = "GET",
-        query   = [  ],
-        headers = [
-          Authorization authString
-          Accept HttpContentTypes.Json
-          UserAgent "GistSync"
-        ])
-
+  let httpRequestAsync url token =
     async {
+
+      let authString =
+        "token " + token
+
+      let httpString =
+        Http.AsyncRequest
+          (url, httpMethod = "GET",
+          query   = [  ],
+          headers = [
+            Authorization authString
+            Accept HttpContentTypes.Json
+            UserAgent "GistSync"
+          ])
+
       try
         let! resp = httpString
-        logger <| sprintf "%A" resp
+        //logger <| sprintf "%A" resp.Headers
+        return (Some (resp.StatusCode, resp.Headers, resp.Body))
       with ex ->
         logger <| sprintf "Error happened while talking to Github API: %s" ex.Message
+        return None
     }
-    |> Async.RunSynchronously
+
+
+  let defaultJsonOptions : JsonSerializerSettings =
+    let contractResolver =
+      DefaultContractResolver(
+        NamingStrategy = SnakeCaseNamingStrategy())
+    JsonSerializerSettings(
+      ContractResolver = contractResolver,
+      DefaultValueHandling = DefaultValueHandling.Ignore)
+
+
+  let getRateLimitFromJson s =
+    try
+      Some (JsonConvert.DeserializeObject<RateLimit>(s, defaultJsonOptions))
+    with ex ->
+      logger <| sprintf "%s" ex.Message
+      None
+
+
+  let logRateLimit (token) =
+    let response =
+      httpRequestAsync "https://api.github.com/rate_limit" token
+      |> Async.RunSynchronously
+    match response with
+    | Some (_httpStatus, _headers, body) ->
+        // logger <| sprintf "%A" body
+        match body with
+        | Text txt ->
+            logger <| sprintf "%A" (getRateLimitFromJson txt)
+        | Binary bin ->
+            logger <| sprintf "%A" (getRateLimitFromJson (Encoding.UTF8.GetString(bin)))
+    | None ->
+        logger <| sprintf "%A" response
+
+
+  let getFirstTwo (xs) =
+    match xs with
+    | x :: y :: _xxs -> Some (x, y)
+    | _ -> None
+
+
+  let swapNext (x:string, y:string) =
+    (((y.Split "=").[1]).Trim('"'), x.Trim('<').Trim('>'))
+
+
+  let tee (log: string -> unit) x =
+    log <| sprintf "%A" x
+    x
+
+
+  let handleNext s =
+    let r = new Regex("(<https://api.github.com/user/[0-9]+/gists\?page=[0-9]+>;\ rel=\"next\")")
+    r.Matches s
+    //|> tee logger
+    |> Seq.tryHead
+    |> Option.map (fun m -> m.Value)
+    |> Option.map (fun x -> x.Split ";")
+    |> Option.map (Seq.map (fun x -> x.Trim(' ')))
+    |> Option.bind (fun xs -> getFirstTwo (Seq.toList xs))
+    |> Option.map swapNext
+
+
+  let getGistFromJson s =
+    try
+      Some (JsonConvert.DeserializeObject<List<Gist>>(s, defaultJsonOptions))
+    with ex ->
+      logger <| sprintf "%s" ex.Message
+      None
+
+
+  let getBodyParts body =
+    match body with
+    | Text txt ->
+        getGistFromJson txt
+    | Binary bin ->
+        getGistFromJson (Encoding.UTF8.GetString(bin))
+
+
+  let isFolder p =
+    try
+      File.GetAttributes(p).HasFlag(FileAttributes.Directory)
+    with ex ->
+      false
+
+
+  let downloadRawUrl token url =
+    let response =
+      httpRequestAsync url token
+      |> Async.RunSynchronously
+    match response with
+    | Some (_httpStatus, _headers, body) ->
+        // logger <| sprintf "%A" body
+        match body with
+        | Text txt ->
+            Some txt
+        | Binary bin ->
+            Some (Encoding.UTF8.GetString(bin))
+    | None ->
+        None
+
+
+  let writeContentToDisk (path:string) (content:string) =
+    try
+      logger <| sprintf "%A" path
+      File.WriteAllText(path, content)
+    with ex ->
+      logger <| sprintf "%A" ex.Message
+
+  let downloadAndSave token folder (f:KeyValuePair<string,FileEntry>) =
+    (downloadRawUrl token f.Value.RawUrl)
+    |> Option.map (writeContentToDisk (Path.Combine(folder, f.Key)))
+    |> ignore
+
+
+  let writeToDisk token (localPath) (g:Gist) =
+    try
+
+      let id = g.Id
+
+      g.Files
+      |> Seq.iter
+        (fun f ->
+
+          logger <| sprintf "localPath: %A id:%A f: %A" localPath id f.Key
+          let folder = Path.Combine(localPath, id)
+
+          match (isFolder folder) with
+
+          | true ->
+            logger <| sprintf "Folder already created"
+            downloadAndSave token folder f
+
+          | false ->
+            logger <| sprintf "Folder nix created yet"
+            let dir = Directory.CreateDirectory(folder)
+            downloadAndSave token folder f
+        )
+    with ex ->
+      logger <| sprintf "%A" ex.Message
+
+
+  let processChunk token (localPath:string) body =
+    getBodyParts body
+    |> Option.map
+      (fun xs ->
+        (Seq.iter (writeToDisk token localPath) xs))
+    |> Option.defaultWith
+      (fun x -> logger <| sprintf "%A" x)
+
+
+  let rec traverseGists localPath (acc:int) url token  =
+    logger <| sprintf "Entering iteration: %i" acc
+    let response =
+      httpRequestAsync url token
+      |> Async.RunSynchronously
+
+    match response with
+    | Some (status, headers, body) ->
+
+        let nextMaybe = handleNext (headers.Item("Link"))
+        //logger <| sprintf "%A" nextMaybe
+
+        match status, nextMaybe with
+        | 200, Some ("next", urlNext) ->
+          processChunk token localPath body
+          logRateLimit token
+          traverseGists localPath (acc+1) urlNext token
+
+        | 503, _  ->
+          logger <| sprintf "You are being throttled!"
+          let waitTime = getWaitTimeExp acc
+          async { do! Async.Sleep(waitTime) }
+          |> Async.RunSynchronously
+          // Retrying with the current context
+          traverseGists localPath acc url token
+
+        | 200, None ->
+          logger <| sprintf "The end."
+          processChunk token localPath body
+          Some acc
+
+        | httpStatus, next ->
+          logger <| sprintf "Potential problem: httpStatus: %A next: %A" httpStatus next
+          None
+
+    | None ->
+        logger <| sprintf "%A" response
+        None
 
 
   let syncGists localPath =
+
+    let initialUrl =
+      sprintf "https://api.github.com/users/%s/gists" "l1x"
+
+    let initialList =
+      new List<Gist>()
+
     readPersonalToken()
-    |> Option.map (httpValami "l1x")
-    |> Option.defaultWith (fun x -> logger <| sprintf "%A" x)
+    |> Option.bind (traverseGists localPath 0 initialUrl)
+    |> Option.map (fun xs -> logger <| sprintf "%A" xs)
+    |> Option.defaultWith (fun xs -> logger <| sprintf "%A" xs)
 
 
   let listGists localPath =
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # GistSync
+
 Syncing gists to local folder
+
+## Wonderful, wonderful life
+
+```json
+{
+	"url": "https://api.github.com/gists/2c193583067cede9b1ab41bebe5646b0",
+	"forks_url": "https://api.github.com/gists/2c193583067cede9b1ab41bebe5646b0/forks",
+	"commits_url": "https://api.github.com/gists/2c193583067cede9b1ab41bebe5646b0/commits",
+	"id": "2c193583067cede9b1ab41bebe5646b0",
+	"node_id": "MDQ6R2lzdDJjMTkzNTgzMDY3Y2VkZTliMWFiNDFiZWJlNTY0NmIw",
+	"git_pull_url": "https://gist.github.com/2c193583067cede9b1ab41bebe5646b0.git",
+	"git_push_url": "https://gist.github.com/2c193583067cede9b1ab41bebe5646b0.git",
+	"html_url": "https://gist.github.com/2c193583067cede9b1ab41bebe5646b0",
+	"files": {
+		"unpriv.docker.md": {
+			"filename": "unpriv.docker.md",
+			"type": "text/markdown",
+			"language": "Markdown",
+			"raw_url": "https://gist.githubusercontent.com/l1x/2c193583067cede9b1ab41bebe5646b0/raw/bd3efd7a96b544b39513690b465701e29eabb523/unpriv.docker.md",
+			"size": 1493
+		}
+	},
+	"public": true,
+	"created_at": "2021-05-14T16:35:41Z",
+	"updated_at": "2021-05-14T16:38:33Z",
+	"description": "",
+	"comments": 0,
+	"user": null,
+	"comments_url": "https://api.github.com/gists/2c193583067cede9b1ab41bebe5646b0/comments",
+	"owner": {
+		"login": "l1x",
+		"id": 20079,
+		"node_id": "MDQ6VXNlcjIwMDc5",
+		"avatar_url": "https://avatars.githubusercontent.com/u/20079?v=4",
+		"gravatar_id": "",
+		"url": "https://api.github.com/users/l1x",
+		"html_url": "https://github.com/l1x",
+		"followers_url": "https://api.github.com/users/l1x/followers",
+		"following_url": "https://api.github.com/users/l1x/following{/other_user}",
+		"gists_url": "https://api.github.com/users/l1x/gists{/gist_id}",
+		"starred_url": "https://api.github.com/users/l1x/starred{/owner}{/repo}",
+		"subscriptions_url": "https://api.github.com/users/l1x/subscriptions",
+		"organizations_url": "https://api.github.com/users/l1x/orgs",
+		"repos_url": "https://api.github.com/users/l1x/repos",
+		"events_url": "https://api.github.com/users/l1x/events{/privacy}",
+		"received_events_url": "https://api.github.com/users/l1x/received_events",
+		"type": "User",
+		"site_admin": false
+	},
+	"truncated": false
+}
+```
